b05b5c89f076ce6b125dca59d6f80118b82af36b
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-23 15:16:01 +0300

    fish frame importance pool sugar

--- user_area/user_login.php
+++ user_area/user_login.php
@@ -58,9 +58,10 @@ if(isset($_POST['user_login'])){
   $result=mysqli_query($con,$select_query);
   $row_count=mysqli_num_rows($result);
   $row_data=mysqli_fetch_assoc($result);
+  $user_ip=getIPAddress();
 
   //cart item
-  $select_query="select * from user_table where username='$username'";
+  $select_query="select * from cart_details where ip_address='$username'";
   if($row_count>0){
     if(password_verify($user_password,$row_data['user_password'])){
       echo "<script>alert('login successful')</script>";
