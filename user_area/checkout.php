f9d9e9eb1d60a60d117bd6e83a2f0d1fdac50b42
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-23 08:08:00 +0300

    gradually pan when escape below

--- user_area/checkout.php
+++ user_area/checkout.php
@@ -91,7 +91,7 @@ include('../includes/connect.php');
 </div>
 <!---footer-->
 <!--include footer-->
-<?php include("footer.php") ?>
+<?php include("../footer.php") ?>
 <!---bootstrap js link---> 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 
   integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
