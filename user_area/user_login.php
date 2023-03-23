a81aa6c5f0fedfb3d75ad155616e52a2d7f876ff
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-23 15:44:01 +0300

    supply swim count fact safe

--- user_area/user_login.php
+++ user_area/user_login.php
@@ -69,7 +69,7 @@ if(isset($_POST['user_login'])){
   $select_cart=mysqli_query($con,$select_query_cart);
   $row_count_cart=mysqli_num_rows($select_cart);
   if($row_count>0){
-    $_SESSION['username']=$user_username;
+    $_SESSION['username']=$username;
     if(password_verify($user_password,$row_data['user_password'])){
       //echo "<script>alert('login successful')</script>";
       if($row_count==1 and $row_count_cart==0){
