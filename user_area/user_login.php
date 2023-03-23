42269cdb48923344431c3c9dec1188b413922713
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-23 14:26:01 +0300

    effort fun great look occur

--- user_area/user_login.php
+++ user_area/user_login.php
@@ -15,7 +15,7 @@
     <h2 class="text-center">User login</h2>
     <div class="row d-flex  align-items-center justify-content-center mt-5">
         <div class="col-lg-12 col-xl-6">
-<form action="" method="post" enctype="multipart/form-data">
+<form action="" method="post">
     <div class="form-outline mb-4">
         <!--username field-->
      <label for="username" class="form-label">UserName</label>
