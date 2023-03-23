5f16277bd98f0530061ad3d4689802d94a2aefd2
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-23 10:04:00 +0300

    herd shall entire room apartment

--- user_area/user_registration.php
+++ user_area/user_registration.php
@@ -97,7 +97,7 @@ if(isset($_POST['user_register'])){
     //inset_query
 move_uploaded_file($user_image_tmp,"./user_image/$user_image");
 $insert_query="insert into user_table (username,user_email,user_password,user_image,user_ip,
-user_address,user_mobile) values ('$user_name','$user_email','$harsh_password'
+user_address,user_mobile) values ('$user_name','$user_email','$hash_password'
 '$user_image','$user_ip','$user_address','$user_contact')";
 $sql_execute=mysqli_query($con,$insert_query);
 if($sql_execute){
