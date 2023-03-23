068042db841478e110db53583de33f76b9c769fb
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-23 15:28:01 +0300

    down warn shelf plane depend

--- user_area/checkout.php
+++ user_area/checkout.php
@@ -81,7 +81,7 @@ include('../includes/connect.php');
     include('user_login.php');
     }
     else{
-        include('../payment.php');
+        include('payment.php');
     }
     ?>
   <!--row-->
