2ba67b60a257308e4d267b7ff288c3a08b0007fb
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-25 07:30:01 +0300

    same break swam youth everything

--- user_area/order.php
+++ user_area/order.php
@@ -10,5 +10,6 @@ $total_price=0;
 $cart_query_price="select *from cart_details 
 where ip_address='$get_ip_address'";
 $result_cart_price=mysqli_query($con,$cart_query_pride);
-
+$
+while($row_price=mysqli_fetch_array($result_cart_price))
 ?>
