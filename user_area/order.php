ca982314bb32865d1252cf7837f51847fd6e7b50
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-25 07:14:01 +0300

    direction soap ranch discovery court

--- user_area/order.php
+++ user_area/order.php
@@ -1,5 +1,8 @@
 <?php
 include('../includes/connect.php');
-include('../functions/common_function.php');
-if(isset($_GET['user_id'])){}
+//include('../functions/common_function.php');
+if(isset($_GET['user_id'])){
+    $user_id=$_GET['user_id'];
+    echo $user_id;
+}
 ?>
