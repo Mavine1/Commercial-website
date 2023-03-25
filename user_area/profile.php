94f3c396f293e5d35d3e328c40f495f8be7ca9b6
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-25 09:32:00 +0300

    egg rocky writer hard hard

--- user_area/profile.php
+++ user_area/profile.php
@@ -107,42 +107,6 @@ cart();
 <div class="row">
    <!--fetching data-->
 
-   <?php
-      getproducts();
-      get_unique_categories();
-      get_unique_brands();
-      //$ip = getIPAddress();  
-     //echo 'User Real IP Address - '.$ip;  
-      ?>
-  <!--row-->
-  </div>
-  <!--col end-->
-</div>
-
-  <div class="col-md-2 bg-secondary p-0">
-    <ul class="navbar-nav me-auto text-center">
-      <li class="nav-item bg-info">
-        <a href="#" class="nav-link text-light"><h4>Delivery Brands</h4></a>
-      </li>
-
-      <!--fetching data-->
-
-      <?php
-      getbrands();
-      ?>
-    </ul>
-  <!------categories--->
-  <ul class="navbar-nav me-auto text-center">
-      <li class="nav-item bg-info">
-        <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
-      </li>
-
-      <!--fetching of data from the database-->
-      <?php
-      getcategories();
-      ?>
-    </ul>
-</div>
 <!---footer-->
 <!--include footer-->
 <?php include("../footer.php") ?>
