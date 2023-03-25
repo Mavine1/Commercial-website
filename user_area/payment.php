51a0375a4d88a5558b6a64e1f44a9804cfea6c10
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-25 05:58:01 +0300

    by pan live cannot thread

--- user_area/payment.php
+++ user_area/payment.php
@@ -12,12 +12,18 @@ include('../functions/common_function.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
+<style>
+    
+</style>
 <body>
    <div class="container">
     <h2 class="text-center text-info">payment options</h2>
     <div class="row">
         <div class="col-md-6">
-        <a href="order.php" >pay offline</a>
+        <a href="order.php" ><img src="../images/apple.png" alt=""></a>
+    </div>
+    <div class="col-md-6">
+        <a href="order.php" ><img src="../images/apple.png" alt=""></a>   
     </div>
     </div>
    </div>
