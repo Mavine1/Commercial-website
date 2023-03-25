321f75ad726de2f43f6f2dc742cf4fab20c19c2a
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-25 07:16:01 +0300

    sail noted particular slight strength

--- user_area/order.php
+++ user_area/order.php
@@ -1,8 +1,8 @@
 <?php
 include('../includes/connect.php');
-//include('../functions/common_function.php');
+include('../functions/common_function.php');
 if(isset($_GET['user_id'])){
     $user_id=$_GET['user_id'];
-    echo $user_id;
+   
 }
 ?>
