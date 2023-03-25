1e67578ee4d3d510b27ac32bdbe183dbd0ed16d2
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-25 09:30:01 +0300

    produce energy symbol parent this

--- user_area/profile.php
+++ user_area/profile.php
@@ -1,7 +1,7 @@
 <!--connect file-->
 <?php
 include('../includes/connect.php');
-include('../functions/common_function.php');
+include('../admin_area/functions/common_function.php');
 session_start();
 ?>
 <!DOCTYPE html>
@@ -145,7 +145,7 @@ cart();
 </div>
 <!---footer-->
 <!--include footer-->
-<?php include("footer.php") ?>
+<?php include("../footer.php") ?>
 <!---bootstrap js link---> 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 
   integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
