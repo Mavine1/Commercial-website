7cba5ecbcbabb717a9cc098cb563b7238a33753f
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-25 21:12:01 +0300

    time five certainly bicycle central

--- user_area/profile.php
+++ user_area/profile.php
@@ -118,7 +118,9 @@ cart();
           $username=$_SESSION['username'];
           $user_image="select * from user_table where username='$username'";
           $result_image=mysqli_query($con,$user_image);
-          $row_image=mysqli_fetch_assoc($user_image);
+          $row_image=mysqli_fetch_array($user_image);
+          
+
 
             ?>
 
