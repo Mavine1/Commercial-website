bb6490a5ec9a22012771445f79f4b435144f8bcb
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-26 23:38:00 +0300

    fog heading impossible oil down

--- user_area/edit_account.php
+++ user_area/edit_account.php
@@ -27,7 +27,7 @@ if(isset($_GET['edit_account'])){
       where user_id=$update_id";
       $result_query_update=mysqli_query($con,$select_query);
       if($result_query_update){
-        echo "<"
+        echo "<script>alert('data  updated successfully')</script>";
       }
 
 
