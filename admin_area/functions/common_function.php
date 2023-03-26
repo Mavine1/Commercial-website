483ac0cde4a45afa8ef18237a8a515a272e470b1
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-26 21:20:00 +0300

    modern hour poem pack teach

--- admin_area/functions/common_function.php
+++ admin_area/functions/common_function.php
@@ -394,8 +394,10 @@ function cart_item(){
             $result_order_query=mysqli_query($con,$get_orders);
             $row_count=mysqli_num_rows($result_order_query);
             if($row_count>0){
-              echo "<h3 class='text-center' text-success nt-5 mb-2' >you have <span class='text-danger'>$row_count</span>pending orders</h3>
+              echo "<h3 class='text-center text-success nt-5 mb-2' >you have <span class='text-danger'>$row_count</span>pending orders</h3>
               <p class='text-center'><a href='profile.php?my_orders' class='text-dark'>Order Details</a></p>";
+            }else{
+              
             }
           }
         }
