77345b80f9963c76bce9c2aa54a686be7ce46455
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-26 23:10:01 +0300

    foreign plus leaving desert square

--- user_area/edit_account.php
+++ user_area/edit_account.php
@@ -28,17 +28,18 @@ if(isset($_GET['edit_account'])){
         <input type="text" class="form-control w-50 m-auto" value="<?php echo $username ?>" name="user_username">
     </div>
     <div class="form-outline mb-4">
-        <input type="email" class="form-control w-50 m-auto" value="<?php echo $username ?> name="user_email">
+        <input type="email" class="form-control w-50 m-auto" value="<?php echo $user_email ?>" name="user_email">
     </div>
     <div class="form-outline mb-4 d-flex w-50 m-auto">
-        <input type="file" class="form-control w-50 m-auto" name="user_image">
+        <input type="file" class="form-control w-50 m-auto" 
+        name="user_image">
       <img src="./user_images/<?php echo $user_image?>" alt="" class="edit_img">
     </div>
     <div class="form-outline mb-4">
-        <input type="text" class="form-control w-50 m-auto" name="user_address">
+        <input type="text" class="form-control w-50 m-auto" value="<?php echo $user_address?>" name="user_address">
     </div>
     <div class="form-outline mb-4">
-        <input type="text" class="form-control w-50 m-auto" name="user_mobile">
+        <input type="text" class="form-control w-50 m-auto" value="<?php echo $username ?>" name="user_mobile">
     </div>
     <input type="submit" value="update" class="bg-info py-2 px-3 border-0" 
     name="user_update">
