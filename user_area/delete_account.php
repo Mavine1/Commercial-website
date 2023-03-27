9f09731d05b41eb5b738ed94610e34c36e2afdb2
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-27 16:34:01 +0300

    chose drink examine path scared

--- user_area/delete_account.php
+++ user_area/delete_account.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Delete Account</title>
-</head>
-<body>
+
     <h3 class="text-danger mb-4">Delete Account</h3>
     <form action="" method="post" class="mt-5">
         <div class="form-outline mb-4">
@@ -18,6 +10,3 @@
             value="Don't Delete Account" >
         </div>
     </form>
-    
-</body>
-</html>
