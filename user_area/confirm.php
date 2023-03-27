e0f9c7331b7a7455e68d2ba2bc66e3d54f8bf5f4
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-27 07:48:01 +0300

    tribe involved top involved shelf

--- user_area/confirm.php
+++ user_area/confirm.php
@@ -4,6 +4,19 @@ include('../includes/connect.php');
 include('../admin_area/functions/common_function.php');
 session_start();
 if(isset($_GET['order_id'])){
-    $order_id=$_GET['order_id']
+    $order_id=$_GET['order_id'];
+    echo $order_id;
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>payment page</title>
+</head>
+<body>
+    
+</body>
+</html>
