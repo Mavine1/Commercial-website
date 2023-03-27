de654d3e31d93d0805c9cc68e39ff89a68889ab9
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-27 23:56:00 +0300

    main selection took wife gray

--- admin_area/edit_products.php
+++ admin_area/edit_products.php
@@ -1,4 +1,8 @@
 <div class="container nt-5">
 <h1 class ="text-center">Edit product</h1>
-<form action="" method="></form>
+<form action="" method="post" enctype ="multipart/form-data">
+    <div class="form_outline">
+        
+    </div>
+</form>
 </div>
