61c70b01190dfbab4a3e99c8f3bf478bde9d7c35
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 00:00:01 +0300

    especially split teacher ten neck

--- admin_area/edit_products.php
+++ admin_area/edit_products.php
@@ -3,7 +3,7 @@
 <form action="" method="post" enctype ="multipart/form-data">
     <div class="form_outline">
         <label for="product_title" class="form-label">product Title</label>
-        <input type="text" id=">
+        <input type="text" id="product_title" name="product_title" class=>
     </div>
 </form>
 </div>
