c99c1c0058618ee6dd11f55c2d4d103965ecf29c
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-27 16:22:00 +0300

    appearance dark understanding combine raise

--- user_area/delete_account.php
+++ user_area/delete_account.php
@@ -8,7 +8,11 @@
 </head>
 <body>
     <h3 class="text-danger mb-4">Delete Account</h3>
-    <form action="" method="post" class="mt-5"></form>
+    <form action="" method="post" class="mt-5">
+        <div class="form-outline mb-4">
+            <input type="submit" class="form-control w-50 m-auto" name=">
+        </div>
+    </form>
     
 </body>
 </html>
