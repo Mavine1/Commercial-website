1afdd315ddd051671d0c107cba92cd5e7c40b21d
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-27 23:58:01 +0300

    fix rubbed stepped ocean applied

--- admin_area/edit_products.php
+++ admin_area/edit_products.php
@@ -2,7 +2,8 @@
 <h1 class ="text-center">Edit product</h1>
 <form action="" method="post" enctype ="multipart/form-data">
     <div class="form_outline">
-        
+        <label for="product_title" class="form-label">product Title</label>
+        <input type="text" id=">
     </div>
 </form>
 </div>
