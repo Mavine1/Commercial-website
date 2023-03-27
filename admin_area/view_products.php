6e6e5203b0a82db97736d6441270d012b9f5cea6
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-27 20:40:00 +0300

    meant adult supply becoming between

--- admin_area/view_products.php
+++ admin_area/view_products.php
@@ -26,14 +26,16 @@
         <?php
         $get_products="select * from products";
         $result=mysqli_query($con,$get_products);
+        $number=0;
         while($row=mysqli_fetch_assoc($result)){
             $product_id=$row['product_id'];
             $product_title=$row['product_title'];
             $product_image=$row['product_image'];
             $product_price=$row['product_price'];
             $status=$row['status'];
-            echo "<tr>
-            <td>1</td>
+            $number++;
+            echo "<tr class='text-center'>
+            <td>$number</td>
             <td>mango</td>
             <td>Image</td>
             <td>444</td>
