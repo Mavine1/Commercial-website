993236959ddcc4f530260d21efe37fe75075f335
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-27 19:48:01 +0300

    development desert aid cowboy stone

--- admin_area/index.php
+++ admin_area/index.php
@@ -1,3 +1,9 @@
+<!--connect file-->
+<?php
+include('../includes/connect.php');
+include('../admin_area/functions/common_function.php');
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
