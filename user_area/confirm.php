ccc479b8019587ec98a585c6bebcf65cedfc9a8a
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-27 07:58:01 +0300

    he mind blood view held

--- user_area/confirm.php
+++ user_area/confirm.php
@@ -19,11 +19,11 @@ if(isset($_GET['order_id'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-<body>
- <h1 class= "text_center text-">Confirm payment</h1>
- <div class="container my-5">
+<body class="bg-secondary">
+<div class="container my-5">
+ <h1 class= "text_center text-light">Confirm payment</h1>
     <form action="" method="post">
-        <div class="form-outline my-4 text-cennter">
+        <div class="form-outline my-4 text-center">
             <input type="text" class="form_control">
         </div>
     </form>
