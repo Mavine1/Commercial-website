eb4bb01525cba7a12eedb9a7d12c35bfb3148712
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-27 13:42:00 +0300

    smoke wonderful zero year cage

--- user_area/confirm.php
+++ user_area/confirm.php
@@ -12,7 +12,7 @@ if(isset($_GET['order_id'])){
     $invoice_number=$row_fetch['invoice_number'];
     $amount_due=$row_fetch['amount_due'];
 }
-
+if(isset($_POST['confirm_payment']))
 ?>
 <!DOCTYPE html>
 <html lang="en">
