445dcbe812cbf98ad28196f0e73d4ebb8920c1c6
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 12:08:00 +0300

    factor drawn being very exchange

--- admin_area/delete_category.php
+++ admin_area/delete_category.php
@@ -4,10 +4,10 @@ if(isset($_GET['delete_category'])){
     //echo $delete_id;
     //delete query
 
-    $delete_products="Delete from products where product_id=$delete_id";
+    $delete_products="Delete from categories where category_id=$delete_category";
     $result_product=mysqli_query($con,$delete_products);
     if($result_product){
-        echo "<script>alert('product update successfully')<script>";
+        echo "<script>alert(' update successfully')<script>";
         echo "<script>window.open('./index.php','_self')<script>";
     }
 }
