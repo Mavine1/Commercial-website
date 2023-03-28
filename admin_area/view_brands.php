dec5ce604aa86c9cc6bf77a5754e190ec703b483
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 12:52:00 +0300

    specific president cream taken actual

--- admin_area/view_brands.php
+++ admin_area/view_brands.php
@@ -42,9 +42,8 @@
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
-      <div class="modal-header">
-      </div>
       <div class="modal-body">
+        <h4>Are you sure you want to delete this </h4>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
