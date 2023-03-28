47230d354a8effac9d45c5d73bb2e76d20d2567d
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 11:02:01 +0300

    energy layers sun feature queen

--- admin_area/edit_category.php
+++ admin_area/edit_category.php
@@ -1,4 +1,6 @@
 <div class="container mt-3">
     <h1 class="text-center">Edit category</h1>
-    <form action=""></form>
+    <form action="" method="post" class ="text-center">
+        <div class="form-outline mb-4 w-50"></div>
+    </form>
 </div>
