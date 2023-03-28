3dff29b3a10f3471846dee1d1e91b5814942f402
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 11:04:00 +0300

    disease aid will fairly taste

--- admin_area/edit_category.php
+++ admin_area/edit_category.php
@@ -1,6 +1,8 @@
 <div class="container mt-3">
     <h1 class="text-center">Edit category</h1>
     <form action="" method="post" class ="text-center">
-        <div class="form-outline mb-4 w-50"></div>
+        <div class="form-outline mb-4 w-50 m-auto">
+            <label for="category_title" class="form-label">category Title</label>
+        </div>
     </form>
 </div>
