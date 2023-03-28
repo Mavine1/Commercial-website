2ae802b9d240f08d87450db26710e7c2f190de26
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 11:26:00 +0300

    according front series pleasure voice

--- admin_area/edit_category.php
+++ admin_area/edit_category.php
@@ -3,7 +3,9 @@ if(isset($_GET['edit_category'])){
     $edit_category=$_GET['edit_category'];
 
     $get_categories="select * from categories where category_id=$edit_category";
-    $result=mysqli_query
+    $result=mysqli_query($con,$get_categories);
+    $row=mysqli_fetch_assoc($result);
+    $category_title=$row[]
 }
 ?>
 <div class="container mt-3">
