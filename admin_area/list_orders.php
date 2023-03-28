81916d8a64b7f76f160f3093d10f386cc872486d
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 13:46:00 +0300

    sitting folks bet object globe

--- admin_area/list_orders.php
+++ admin_area/list_orders.php
@@ -1,10 +1,16 @@
-<h3 class= "text-center text-success">All </h3>
+<h3 class= "text-center text-success">All Orders</h3>
 <table class="table table-bordered mt-5">
     <thead class="bg-info">
         <tr class ="text-center">
+
+        <?php
+        $get_orders+"select * fe"
+        ?>
             <th>S1 no</th>
             <th>Due Amount</th>
             <th>invoice number</th>
             <th>Total products</th>
+            <th>Order data</th>
+            <th>Status</th>
         </tr>
     </thead>
