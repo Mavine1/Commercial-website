14c95fc60c3374f11dd63c0e3b6c886ff74872b0
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 10:18:01 +0300

    property upper while class sell

--- admin_area/view_categories.php
+++ admin_area/view_categories.php
@@ -13,7 +13,7 @@
             <td>1</td>
             <td>Juices</td>
             <td><a href='' class='text-light'><i class='fa-solid fa-pen-to-square'></i></td>
-           <td 
+           <td><a href=''</td> 
             
         </tr>
     </tbody>
