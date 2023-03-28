3caa563bb0f6a63ac83b9a6e8c7594c3b7582188
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 11:46:00 +0300

    roll living community turn ourselves

--- admin_area/edit_brands.php
+++ admin_area/edit_brands.php
@@ -1,33 +1,33 @@
 <?php
-if(isset($_GET['edit_category'])){
-    $edit_category=$_GET['edit_category'];
+if(isset($_GET['edit_brands'])){
+    $edit_brands=$_GET['edit_brands'];
 
-    $get_categories="select * from categories where category_id=$edit_category";
+    $get_categories="select * from categories where brands_id=$edit_brands";
     $mysqli_result=mysqli_query($con,$get_categories);
     $row=mysqli_fetch_array($mysqli_result);
-    $category_title=$row['category_title'];
+    $brands_title=$row['brands_title'];
 }
 
 if(isset($_POST['edit_cat'])){
-    $cat_title=$_POST["category_title"];
-    $update_query="update categories set category_title='cat_title' where
-    category_id=$edit_category";
+    $cat_title=$_POST["brands_title"];
+    $update_query="update categories set brands_title='cat_title' where
+    brands_id=$edit_brands";
     $result_cat=mysqli_query($con,$update_query);
     if($result_cat){
-        echo "<script>alert('category has been updated successfully')</script>";
+        echo "<script>alert('brands has been updated successfully')</script>";
         echo "<script>window.open('.index.php?/view_categories.php','_self')</script>";
     }
 }
 ?>
 <div class="container mt-3">
-    <h1 class="text-center">Edit category</h1>
+    <h1 class="text-center">Edit brands</h1>
     <form action="" method="post" class ="text-center">
         <div class="form-outline mb-4 w-50 m-auto">
-            <label for="category_title" class="form-label">category Title</label>
-            <input type="text" name="category_title" id="category_title" class="form-control"
-            required="required"  value='<?php echo $category_title;?>'>
+            <label for="brands_title" class="form-label">brands Title</label>
+            <input type="text" name="brands_title" id="brands_title" class="form-control"
+            required="required"  value='<?php echo $brands_title;?>'>
         </div>
-        <input type="submit" value="update Category" class="btn btn-info px-3 mb-3"\
+        <input type="submit" value="update brands" class="btn btn-info px-3 mb-3"\
         name="edit_cat">
     </form>
 </div>
