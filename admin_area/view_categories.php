7b022dc59a296828c454259cefd917dcbbff21df
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 10:22:00 +0300

    walk result chemical additional yard

--- admin_area/view_categories.php
+++ admin_area/view_categories.php
@@ -1,7 +1,7 @@
 <h3 class= "text-center text-success">All Categories</h3>
 <table class="table table-bordered mt-5">
     <thead class="bg-info">
-        <tr>
+        <tr class >
             <th>S1no</th>
             <th>Category title</th>
             <th>Edit</th>
