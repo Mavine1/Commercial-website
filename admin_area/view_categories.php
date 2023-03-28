8b19a82fbfa39f70685ca42e751668200d159ce9
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 10:54:00 +0300

    diagram average cannot minute most

--- admin_area/view_categories.php
+++ admin_area/view_categories.php
@@ -23,7 +23,7 @@
         <tr class="bg-secondary text-light">
             <td><?php echo $number;?></td>
             <td><?php echo $category_title;?></td>
-            <td><a href='index.php?edit_category=<?php echo $category_id ?>' class='text-light'><i class='fa-solid fa-pen-to-square'></i></td>
+            <td><a href='index.php?edit_category=<?php echo $category_id?>' class='text-light'><i class='fa-solid fa-pen-to-square'></i></td>
            <td><a href=''class='text-light'><i class='fa-solid fa-trash'></i>
            </a></td> 
             
