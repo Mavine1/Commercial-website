ca2c5d2b4a56023e98dd2fd109274eb585a94cc2
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 14:16:01 +0300

    tall nodded grass imagine six

--- admin_area/list_orders.php
+++ admin_area/list_orders.php
@@ -28,17 +28,17 @@ if($row_count==0){
         $amount_due=$row_data['amount_due'];
         $invoice_number=$row_data['invoice_number'];
         $total_products=$row_data['total_products'];
-        $order_id=$row_data['order_id'];
+        $order_date=$row_data['order_date'];
         $order_status=$row_data['$order_status'];
         $number++;
         echo " <tr>
         <td>$number</td>
         <td>$amount_due</td>
-        <td>$in</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>$invoice_number</td>
+        <td>$total_products</td>
+        <td>$order_date</td>
+        <td>$order_status</td>
+        <td>delete</td>
     </tr>";
     }
 }
