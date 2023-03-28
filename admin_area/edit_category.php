7503346f12e5d9bd7266ec1b8b2524365d45a8aa
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 11:16:01 +0300

    cross substance chief personal idea

--- admin_area/edit_category.php
+++ admin_area/edit_category.php
@@ -5,9 +5,8 @@
         <div class="form-outline mb-4 w-50 m-auto">
             <label for="category_title" class="form-label">category Title</label>
             <input type="text" name="category_title" id="category_title" class="form-control"
-            required="required">
+            required="required"  value='<?php echo $category_title;?>'>
         </div>
-        <input type="submit" value="update Category" class="btn btn-info px-3 mb-3"
-        required="required" value='<?php echo $category_title;?>'>
+        <input type="submit" value="update Category" class="btn btn-info px-3 mb-3">
     </form>
 </div>
