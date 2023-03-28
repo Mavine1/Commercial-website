eddd03c6c1791b4da0c6b27a7a6acb8c41557d87
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 10:42:01 +0300

    building nails apartment travel behind

---
+++ admin_area/view_brands.php
@@ -0,0 +1,34 @@
+<h3 class= "text-center text-success">All Categories</h3>
+<table class="table table-bordered mt-5">
+    <thead class="bg-info">
+        <tr class ="text-center">
+            <th>S1no</th>
+            <th>Brand title</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody class="bg-secondary text-light">
+    <?php
+    $select_cat="select * from brands ";
+    $result=mysqli_query($con,$select_cat);
+    $number=0;
+    while($row=mysqli_fetch_assoc($result)){
+        $brand_id=$row['category_id'];
+        $brand_title=$row['category_title'];
+        $number++;
+
+    
+    ?>
+        <tr class="bg-secondary text-light">
+            <td><?php echo $number;?></td>
+            <td><?php echo $category_title;?></td>
+            <td><a href='' class='text-light'><i class='fa-solid fa-pen-to-square'></i></td>
+           <td><a href=''class='text-light'><i class='fa-solid fa-trash'></i>
+           </a></td> 
+            
+        </tr>
+        <?php
+    }?>
+    </tbody>
+</table>
