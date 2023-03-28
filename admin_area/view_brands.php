6fc22012176caf4a933e06f363d6e9b4b1192406
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 10:44:00 +0300

    wolf against knew paragraph forty

--- admin_area/view_brands.php
+++ admin_area/view_brands.php
@@ -1,4 +1,4 @@
-<h3 class= "text-center text-success">All Categories</h3>
+<h3 class= "text-center text-success">All brands</h3>
 <table class="table table-bordered mt-5">
     <thead class="bg-info">
         <tr class ="text-center">
@@ -14,15 +14,15 @@
     $result=mysqli_query($con,$select_cat);
     $number=0;
     while($row=mysqli_fetch_assoc($result)){
-        $brand_id=$row['category_id'];
-        $brand_title=$row['category_title'];
+        $brand_id=$row['brand_id'];
+        $brand_title=$row['brand_title'];
         $number++;
 
     
     ?>
         <tr class="bg-secondary text-light">
             <td><?php echo $number;?></td>
-            <td><?php echo $category_title;?></td>
+            <td><?php echo $brand_title;?></td>
             <td><a href='' class='text-light'><i class='fa-solid fa-pen-to-square'></i></td>
            <td><a href=''class='text-light'><i class='fa-solid fa-trash'></i>
            </a></td> 
