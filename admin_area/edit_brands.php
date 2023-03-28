359b0a38a3ff6ba4dec97e8ed17016764a386c55
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 11:54:00 +0300

    purple double force carbon freedom

--- admin_area/edit_brands.php
+++ admin_area/edit_brands.php
@@ -8,7 +8,7 @@ if(isset($_GET['edit_brands'])){
     $brand_title=$row['brand_title'];
 }
 
-if(isset($_POST['edit_cat'])){
+if(isset($_POST['edit_brand'])){
     $cat_title=$_POST["brand_title"];
     $update_query="update categories set brands_title='cat_title' where
     brands_id=$edit_brands";
@@ -20,14 +20,14 @@ if(isset($_POST['edit_cat'])){
 }
 ?>
 <div class="container mt-3">
-    <h1 class="text-center">Edit brands</h1>
+    <h1 class="text-center">Edit brand</h1>
     <form action="" method="post" class ="text-center">
         <div class="form-outline mb-4 w-50 m-auto">
-            <label for="brands_title" class="form-label">brands Title</label>
-            <input type="text" name="brands_title" id="brands_title" class="form-control"
+            <label for="brand_title" class="form-label">brands Title</label>
+            <input type="text" name="brands_title" id="brand_title" class="form-control"
             required="required"  value='<?php echo $brand_title;?>'>
         </div>
-        <input type="submit" value="update brands" class="btn btn-info px-3 mb-3"\
-        name="edit_cat">
+        <input type="submit" value="update brand" class="btn btn-info px-3 mb-3"\
+        name="edit_brand">
     </form>
 </div>
