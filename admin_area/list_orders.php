64e5146a390733ebdd444a6c44e6f27724eceae0
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 13:44:01 +0300

    thou environment honor fight cookies

---
+++ admin_area/list_orders.php
@@ -0,0 +1,10 @@
+<h3 class= "text-center text-success">All </h3>
+<table class="table table-bordered mt-5">
+    <thead class="bg-info">
+        <tr class ="text-center">
+            <th>S1 no</th>
+            <th>Due Amount</th>
+            <th>invoice number</th>
+            <th>Total products</th>
+        </tr>
+    </thead>
