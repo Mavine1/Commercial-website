9c48c5024408a4771de9ce5f89ce30d5f471bc22
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 12:06:01 +0300

    lack way nearest practice still

--- admin_area/delete_category.php
+++ admin_area/delete_category.php
@@ -1,1 +1,13 @@
-hello
+<?php
+if(isset($_GET['delete_category'])){
+    $delete_id=$_GET['delete_category'];
+    //echo $delete_id;
+    //delete query
+
+    $delete_products="Delete from products where product_id=$delete_id";
+    $result_product=mysqli_query($con,$delete_products);
+    if($result_product){
+        echo "<script>alert('product update successfully')<script>";
+        echo "<script>window.open('./index.php','_self')<script>";
+    }
+}
