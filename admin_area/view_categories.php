6821196ac468d900a1aee8e8649de3adae606141
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 10:08:00 +0300

    watch farm note flag adjective

--- admin_area/view_categories.php
+++ admin_area/view_categories.php
@@ -1,4 +1,12 @@
 <h3 class= "text-center text-success">All Categories</h3>
 <table class="table table-bordered mt-5">
-    <thead
+    <thead class="bg-info">
+        <tr>
+            <th>S1no</th>
+            <th>Category title</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+    </thead>
+    <tbody
 </table>
