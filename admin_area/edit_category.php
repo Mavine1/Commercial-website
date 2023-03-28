b9a484e3f2210d6b1ce8f42f47a7b346bdbc3d82
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 11:06:01 +0300

    minerals easy wise nearest properly

--- admin_area/edit_category.php
+++ admin_area/edit_category.php
@@ -3,6 +3,7 @@
     <form action="" method="post" class ="text-center">
         <div class="form-outline mb-4 w-50 m-auto">
             <label for="category_title" class="form-label">category Title</label>
+            <input type="text" name="category_title" id="category_title">
         </div>
     </form>
 </div>
