d6cccaacd27ef2f50be95595ce1032ace1526390
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 11:08:01 +0300

    rise face poem except cowboy

--- admin_area/edit_category.php
+++ admin_area/edit_category.php
@@ -3,7 +3,9 @@
     <form action="" method="post" class ="text-center">
         <div class="form-outline mb-4 w-50 m-auto">
             <label for="category_title" class="form-label">category Title</label>
-            <input type="text" name="category_title" id="category_title">
+            <input type="text" name="category_title" id="category_title" class="form-control"
+            required="required">
         </div>
+        <input type="submit" value
     </form>
 </div>
