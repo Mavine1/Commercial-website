fb583ed4e7d3a236064ce3e9e2a3dbf243d4001e
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 10:24:00 +0300

    stomach by summer radio summer

--- admin_area/view_categories.php
+++ admin_area/view_categories.php
@@ -1,7 +1,7 @@
 <h3 class= "text-center text-success">All Categories</h3>
 <table class="table table-bordered mt-5">
     <thead class="bg-info">
-        <tr class >
+        <tr class ="text-center">
             <th>S1no</th>
             <th>Category title</th>
             <th>Edit</th>
