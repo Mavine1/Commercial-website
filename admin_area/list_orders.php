48182e0f0609753d719b991edb3076f5b62a1f38
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-28 13:48:01 +0300

    cow rubbed behind father classroom

--- admin_area/list_orders.php
+++ admin_area/list_orders.php
@@ -1,11 +1,13 @@
 <h3 class= "text-center text-success">All Orders</h3>
 <table class="table table-bordered mt-5">
     <thead class="bg-info">
-        <tr class ="text-center">
-
+       
         <?php
-        $get_orders+"select * fe"
+        $get_orders+"select * from user_orders";
+        $result=mysqli_query($con,$get_orders);
+        $row_count=mysqli_num_rows()
         ?>
+         <tr class ="text-center">
             <th>S1 no</th>
             <th>Due Amount</th>
             <th>invoice number</th>
