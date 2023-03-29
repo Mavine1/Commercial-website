1bc4bcc769c675bd47194e4611497fe43ce0cc71
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-29 07:18:00 +0300

    fix dangerous tell from trade

--- user_area/user_registration.php
+++ user_area/user_registration.php
@@ -111,7 +111,7 @@ else{
 $insert_query="insert into user_table (username,user_email,user_password,user_image,user_ip,
 user_address,user_mobile) values ('$user_username','$user_email','$hash_password',
 '$user_image','$user_ip','$user_address','$user_contact')";
-$sql_execute=mysqli_query($con,$insert_query);
+$result_query=mysqli_query($con,$insert_query);
 if($result_query){
     echo "<script>alert('inserted successfully')</script>";
    }    
